9504d9beb787aa79e3e87ad157c2eb51ca315250
AJITH1509 <ajithmuthu799@gmail.com>
2023-05-09 18:07:31 +0530

    get end point added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -11,8 +12,29 @@ export const client = new MongoClient(MONGO_URL); // dial
 // Top level await
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
-
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("vanakamda mapla");
+  res.send("welcome to E kart");
+});
+app.get("/products", async (req, res) => {
+  try {
+    const getdata = await client
+      .db("b42wd2")
+      .collection("Eproducts")
+      .find({})
+      .toArray();
+    res.send(getdata);
+  } catch (e) {
+    res.send("internal server error");
+  }
+});
+app.post("/add/data", async (req, res) => {
+  const data = req.body;
+  const addData = await client
+    .db("b42wd2")
+    .collection("Eproducts")
+    .insertMany(data);
+  res.send("inserted succcesfully");
 });
 app.listen(Port, () => console.log(`${Port} is running`));
