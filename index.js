e5e853f3a01758767b06475b4fc1e88c2d447582
AJITH1509 <ajithmuthu799@gmail.com>
2023-05-10 18:53:20 +0530

    add cart endpoint added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -37,4 +38,48 @@ app.post("/add/data", async (req, res) => {
     .insertMany(data);
   res.send("inserted succcesfully");
 });
+
+app.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  const data = {
+    name: name,
+    password: password,
+    cart: [],
+  };
+  const storeUser = client.db("b42wd2").collection("Eusers").insertOne(data);
+  res.send("inserted succcesfully");
+});
+app.get("/cart/:user", async (req, res) => {
+  const { user } = req.params;
+  const User = await client
+    .db("b42wd2")
+    .collection("Eusers")
+    .findOne({ _id: new ObjectId(user) });
+  res.status(200).send(User);
+});
+app.put("/addtocart/:id/:user", async (req, res) => {
+  const { id, user } = req.params;
+  try {
+    const checkData = await client
+      .db("b42wd2")
+      .collection("Eproducts")
+      .findOne({ _id: new ObjectId(id) });
+    const checkUser = await client
+      .db("b42wd2")
+      .collection("Eusers")
+      .findOne({ _id: new ObjectId(user) });
+    const getCart = checkUser.cart;
+    const addcart = {
+      cart: [...getCart, checkData],
+    };
+    const addCart = await client
+      .db("b42wd2")
+      .collection("Eusers")
+      .updateOne({ _id: new ObjectId(user) }, { $set: addcart });
+    res.status(200).send({ message: "updated successfully" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.listen(Port, () => console.log(`${Port} is running`));
